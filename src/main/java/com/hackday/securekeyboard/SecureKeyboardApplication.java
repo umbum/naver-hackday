507d530eb376484a77c8caafc58ccf469d2fc1b7
umbum <umbum7601@gmail.com>
2019-05-13 16:02:13 +0900

    init

---
+++ src/main/java/com/hackday/securekeyboard/SecureKeyboardApplication.java
@@ -0,0 +1,13 @@
+package com.hackday.securekeyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureKeyboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureKeyboardApplication.class, args);
+    }
+
+}
